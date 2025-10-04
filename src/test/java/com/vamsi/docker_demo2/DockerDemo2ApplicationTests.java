d333f4060a01d120a2598561f5e6a7121ce841e9
Vamsikirankallem <kallemvamsikiran@gmail.com>
2025-10-04 11:26:18 +0530

    testing docker

---
+++ src/test/java/com/vamsi/docker_demo2/DockerDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vamsi.docker_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
